0856a206d16615e65a81e68ca9361de6eb4b2fc2
minkj1992 <minkj1992@gmail.com>
2019-08-30 14:17:50 +0900

    ch02 summary

---
+++ .simple.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/minkj1992/code/os/simple.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/minkj1992/code/os/simple.ko /home/minkj1992/code/os/simple.o /home/minkj1992/code/os/simple.mod.o ;  true
---
+++ .tmp_versions/simple.mod
@@ -0,0 +1,3 @@
+/home/minkj1992/code/os/simple.ko
+/home/minkj1992/code/os/simple.o
+
