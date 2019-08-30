5d8537a1651ae9fd355cb67730a4cf9020ffc0dd
minkj1992 <minkj1992@gmail.com>
2019-08-30 14:21:19 +0900

    clean

--- .simple.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/minkj1992/code/os/simple.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/minkj1992/code/os/simple.ko /home/minkj1992/code/os/simple.o /home/minkj1992/code/os/simple.mod.o ;  true
--- .tmp_versions/simple.mod
+++
@@ -1,3 +0,0 @@
-/home/minkj1992/code/os/simple.ko
-/home/minkj1992/code/os/simple.o
-
